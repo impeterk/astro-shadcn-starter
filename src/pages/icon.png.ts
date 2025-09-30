55d9c89a42e0bf8f7fb13034455be7651ecb02cf
peterkudelas@gmail.com <peterkudelas@gmail.com>
2025-09-30 22:48:46 +0200

    here in this crazy

--- src/pages/icon.png.ts
+++ src/pages/icon.png.ts
@@ -6,30 +6,15 @@ export const size = {
   height: 32,
 };
 export async function GET() {
-  const res = html(` <div
-  key="mainDiv"
-  style="
-  font-size: 42px;
-  background: white;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color:rebeccapurple;
-  border-radius: 30%;
-"
-  >
-    <svg fill="none" viewBox="0 0 32 32" height="1em" width="1em" key="svg">
-      <path
-        key="path"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-      />
-    </svg>
-  </div>`);
+  const res =
+    html(` <div key="mainDiv" tw="bg-slate-100 w-full h-full flex items-center justify-center text-amber-500 rounded-lg"
+   style="font-size: 42px;">
+   <svg fill="none" viewBox="0 0 32 32" height="1em" width="1em" key="svg">
+     <path key="path"
+       d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+       fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" />
+   </svg>
+ </div>`);
 
   // Typescript hax
   return new ImageResponse(res as unknown as ReactElement, { ...size });
--- src/pages/og-image.png.ts
+++ src/pages/og-image.png.ts
@@ -8,18 +8,17 @@ export const size = {
 };
 export async function GET({ request }: { request: Request }) {
   const icon = new URL("/icon.png", request.url);
-  const res = html(` <div
-            tw="relative flex h-full w-full items-center justify-center bg-gray-900"
-          >
-            <div tw="top-12 left-12 absolute flex items-center">
-              <img tw="w-12 h-12" src="${icon.href}" />
-              <div tw="font-semibold text-slate-200 ml-10 text-2xl ">${BRAND_NAME}</div>
-            </div>
-            <div tw="flex flex-col items-center ">
-              <div tw="text-8xl font-semibold text-slate-300  text-center max-w-2xl mb-12">${SITE_TITLE}</div>
-              <div tw="text-8xl font-medium text-slate-300  text-center ">Astro + Shadcn + React</div>
-            </div>
-          </div>`);
+  const res =
+    html(`<div tw="relative flex h-full w-full items-center justify-center bg-gray-900">
+  <div tw="top-12 left-12 absolute flex items-center">
+    <img tw="w-12 h-12" src="${icon.href}" />
+    <div tw="font-semibold text-slate-200 ml-10 text-2xl ">${BRAND_NAME}</div>
+  </div>
+  <div tw="flex flex-col items-center ">
+    <div tw="text-8xl font-semibold text-slate-300  text-center max-w-2xl mb-12">${SITE_TITLE}</div>
+    <div tw="text-8xl font-medium text-slate-300  text-center ">Astro + Shadcn + React</div>
+  </div>
+</div>`);
 
   // Typescript hax
   return new ImageResponse(res as unknown as ReactElement, { ...size });
