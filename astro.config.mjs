d1a1a1ee80721dbb4d61f2e933a5bd2101eb7555
peterkudelas@gmail.com <peterkudelas@gmail.com>
2026-02-01 00:50:10 +0100

    clean NPM i

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,11 +15,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE_URL,
   integrations: [mdx(), sitemap(), react(), icon()],
-  server: {
-    port: Number(PORT),
-  },
   vite: {
     plugins: [tailwindcss()],
   },
