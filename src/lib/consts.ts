6090433afc73b1afe17119948e129f351275b1d9
peterkudelas@gmail.com <peterkudelas@gmail.com>
2026-02-01 01:38:28 +0100

    change consts

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,17 +1,5 @@
-import { loadEnv } from "vite";
-export const { SITE_URL = "https://astro-starter.peterk.dev" } = loadEnv(
-  process.env.NODE_ENV!,
-  process.cwd(),
-  "",
-);
-console.log({ SITE_URL });
-
-export const { PORT = 4321 } = loadEnv(
-  process.env.NODE_ENV!,
-  process.cwd(),
-  "",
-);
-
+export const  SITE_URL  =  "https://astro-starter.peterk.dev" 
+export const  PORT = 4321 ;
 export const SITE_TITLE = "Astro Starter";
 export const SITE_DESCRIPTION = "astro + react + shadcn starter";
 export const BRAND_NAME = "Peter.dev Astro starter";
